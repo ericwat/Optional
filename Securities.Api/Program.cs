111ce141739fc2588bff5e51dd70aaf5b429e4da
Eric Watkins <ericwat45660@gmail.com>
2024-04-03 19:46:41 -0400

    include project files missing from initial commit

---
+++ Securities.Api/Program.cs
@@ -0,0 +1,27 @@
+using Securities.Api;
+using Securities.Lib;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ISecuritiesService, SecuritiesService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Configure endpoints
+Endpoints.ConfigureWeatherForecastEndpoint(app);
+Endpoints.ConfigureSecuritiesEndpoint(app);
+
+app.Run();
+
